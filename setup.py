079646f5bc3e8f39a3655f0f17899033b09f5984
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-07 12:34:33 +0300

    Initialise Python package
    Co-authored-by: Kelvin Jayanoris <kjayanoris@gmail.com>
    Co-authored-by: Janette Wambere <janette@wambere.com>

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""
+Setup.py for django-vega-admin
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='superset-patchup',
+    version="0.0.1",
+    description='Superset blueprint for Canopy - https://canopyinsights.com',
+    license='Apache2',
+    author='Ona Systems Inc',
+    author_email='superset-patchup+tech@ona.io',
+    url='https://github.com/onaio/superset-patchup',
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=[
+        'Superset',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
