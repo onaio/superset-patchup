e3132da088396673dc9efe74d7d6a676121c3ffe
Greg Studer <gstuder@ona.io>
2019-08-06 12:10:56 +0300

    Initial import breaks packaging steps, do later

--- superset_patchup/__init__.py
+++ superset_patchup/__init__.py
@@ -1,11 +1,13 @@
 """
 Main init file for superset_patchup
 """
-from . import oauth
-from . import views
 from .version import (VERSION, __version__)
 
 #pylint: disable=unused-argument
 def patch_app(superset_app):
     """Hook called once superset is initialized"""
+    # DRAGONS - import here, otherwise we add dependencies for packaging which
+    # needs __version__
+    from . import views
+    # NOTE: Is there a way to get the superset instance from the app obj?
     views.patch_views()
