be148791063eeda72df76a36a0d934d723fda3ee
Ukang'a Dickson <ukanga@users.noreply.github.com>
2020-06-26 05:48:06 +0300

    Set scheme in url_for() calls

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,8 +5,26 @@ Ketchup test config
 
 import superset_patchup
 
+from flask_appbuilder.security.manager import AUTH_OAUTH
 from superset import config as ketchup_config
 
 ketchup_config.WTF_CSRF_ENABLED = False
 ketchup_config.TESTING = True
+ketchup_config.SECRET_KEY = 'abc'
 superset_patchup.add_ketchup(ketchup_config)
+SECRET_KEY = ketchup_config.SECRET_KEY
+AUTH_TYPE = AUTH_OAUTH
+OAUTH_PROVIDERS = [
+    {
+        'name': 'onadata',
+        'remote_app': {
+            'consumer_key': 'consumer key goes here',
+            'consumer_secret': 'consumer secret goes here',
+            'base_url': 'https://stage-api.ona.io/',
+            'access_token_url': 'https://stage-api.ona.io/o/token/',
+            'authorize_url': 'https://stage-api.ona.io/o/authorize/',
+        }
+    }
+]
+SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
